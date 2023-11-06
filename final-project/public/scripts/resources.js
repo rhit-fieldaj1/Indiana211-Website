ce117f6c93714655743539937f464e0ebd442d44
fieldaj1 <fieldaj1@rose-hulman.edu>
2023-11-06 14:37:08 -0500

    Zip code API fetch

--- final-project/public/scripts/resources.js
+++ final-project/public/scripts/resources.js
@@ -1,8 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
 
     const zipCode = localStorage.getItem('zipCode');
+    const coordinates = await getCoordinates(zipCode);
+
     const foodPantryLink = document.getElementById('foodPantry');
 
+    
 
 
     foodPantryLink.addEventListener('click', getFoodPantries);
@@ -22,5 +25,23 @@ getFoodPantries = async function() {
         console.log(resources[0]);
     } catch (error) {
         console.error('Error fetching resources: ', error);
+    }
+}
+
+
+getCoordinates = async function (zipCode) {
+    const apiUrl = `https://thezipcodes.com/api/v1/search?zipCode=${zipCode}&countryCode=US&state=Indiana&apiKey=dc99a5a04b7d10ac5b710acd89fd5e5d`;
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return {
+            latitude: data.location[0].latitude,
+            longitude: data.location[0].longitude
+        }
+    } catch (error) {
+        console.error('Error fetching zip code location: ', error);
     }
 }
